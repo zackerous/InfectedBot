b7932065ce9c10b3b568d22b51f5426d26ebd379
zackerous <cenejenko@gmail.com>
2015-05-12 16:32:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1570,18 +1570,20 @@
                 }
             },
 
-            onlineUsersCommand: {
-                command: ['onlineUsers'],
-                rank: 'bouncer',
+            gandalfCommand: {
+                command: ['gandalf'],
+                rank: 'cohost',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                 	if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
 if (!basicBot.commands.executable(this.rank, chat)) return void (0);
 else {
-API.getUsers(function(users) {
-      API.sendChat("Number of users in the room: " + users.length);
-    });
-    var users = API.getUsers();
+			var msg = chat.message;
+                        if (msg.length === cmd.length) return API.sendChat(subChat(basicBot.chat.nouserspecified, {name: chat.un}));
+                        var name = msg.substr(cmd.length + 2);
+                        var user = basicBot.userUtilities.lookupUserName(name);
+                        if (typeof user === 'boolean') return API.sendChat(subChat(basicBot.chat.invaliduserspecified, {name: chat.un}));
+                        API.sendChat('/me You shall not pass, ' + user.id)
 }
 }
             },
